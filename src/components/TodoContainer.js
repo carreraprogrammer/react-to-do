f108d2cc5a04e267373868eb36f8618ede84d0b5
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-11-07 20:31:55 -0500

    Add diagram and display list on the screen

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,12 +1,32 @@
 import React from "react"
 class TodoContainer extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello from Create React App</h1>
-        <p>I am in a React Component!</p>
-      </div>
-    )
-  }
+    state = {
+        todos: [
+          {
+            id: 1,
+            title: "Setup development environment",
+            completed: true
+          },
+          {
+            id: 2,
+            title: "Develop website and add content",
+            completed: false
+          },
+          {
+            id: 3,
+            title: "Deploy to live server",
+            completed: false
+          }
+        ]
+       };
+       render() {
+        return (
+          <ul>
+            {this.state.todos.map(todo => (
+              <li>{todo.title}</li>
+            ))}
+          </ul>
+        );
+      }
 }
 export default TodoContainer
