850737f7b2b2113911f4405ab5b4f030d3f126fa
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-11-08 18:18:45 -0500

    Add an input to edit the task

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,28 +1,59 @@
-import React from "react"
+import { useState } from 'react';
 import styles from "./TodoItem.module.css"
 
+
 function TodoItem(props) {
+  const [state, setState] = useState({
+    editing: false,
+  });
+
+  const handleEditing = () => {
+   setState({
+    editing: true,
+   });
+  }
+
  const completedStyle = {
-        fontStyle: "italic",
-        color: "#595959",
-        opacity: 0.4,
-        textDecoration: "line-through",
-      }
+   fontStyle: "italic",
+   color: "#595959",
+   opacity: 0.4,
+   textDecoration: "line-through",
+  }
+    
+  const { completed, id, title } = props.todo
 
-      const { completed, id, title } = props.todo
+  let viewMode = {}
+  let editMode = {}
 
-      return (
-        <li className={styles.item}>
-          <input
-            type="checkbox"
-            className={styles.checkbox}
-            checked={completed}
-            onChange={() => props.handleChangeProps(id)}
-          />
-          <button onClick={() => props.deleteTodoProps(id)}>Delete</button>
-          <span style={completed ? completedStyle : null}>{title}</span>
-        </li>
-      )
+  if (state.editing === true) {
+    viewMode.display = "none"
+  } else {
+    editMode.display = "none"
+  }
+
+  return (
+    <li className={styles.item}>
+      <div onDoubleClick={handleEditing}  style={viewMode}>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={completed}
+          onChange={() => props.handleChangeProps(id)}
+        />
+        <button onClick={() => props.deleteTodoProps(id)}>Delete</button>
+        <span style={completed ? completedStyle : null}>{title}</span>
+      </div>
+      <input 
+        type="text" 
+        className={styles.textInput} 
+        style={editMode} 
+        value={title}
+        onChange={e => {
+            console.log(e.target.value, id)
+          }}
+      />
+    </li>
+  )
 }
 
 export default TodoItem
