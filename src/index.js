ddee5b66174b66dffd1480b8c47eee15fefc4ae0
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-11-07 19:24:15 -0500

    Wrap TodoContainer within the StrictMode

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import React from "react"
 import ReactDOM from "react-dom"
 //component file
 import TodoContainer from "./components/TodoContainer"
-ReactDOM.render(<TodoContainer />, document.getElementById("root"))
+ReactDOM.render(
+    <React.StrictMode>
+      <TodoContainer />
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
