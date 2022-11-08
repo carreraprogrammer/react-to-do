a3adfd976800e6cbd22315a48d299f7124a35f32
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-11-08 15:12:05 -0500

    import base styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react"
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 //component file
-import TodoContainer from "./components/TodoContainer"
+import TodoContainer from "./components/TodoContainer";
+
+//stylesheet
+import "./App.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
