9a6bd7d102423da98aa3d39884ece0c4a5e379ce
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-11-07 21:10:16 -0500

    Fix error ReactDOM is not longer supported

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client"
 //component file
 import TodoContainer from "./components/TodoContainer"
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
     <React.StrictMode>
       <TodoContainer />
-    </React.StrictMode>,
-    document.getElementById("root")
+    </React.StrictMode>
   )
