95404ca21c0e78e8cc519a43c20aefbf789d3c94
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-11-09 08:51:20 -0500

    Add componentDidUpdate method

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -50,6 +50,12 @@ class TodoContainer extends React.Component {
           }),
         })
       };
+      componentDidUpdate(prevProps, prevState) {
+        if(prevState.todos !== this.state.todos) {
+          const temp = JSON.stringify(this.state.todos)
+          localStorage.setItem("todos", temp)
+        }
+      }
 
       render() {
         return (
