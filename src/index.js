e94ddd06f0b17fd245b9d0d7fbf489b0f62a625a
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-11-09 14:47:50 -0500

    Add BrowserROuter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom/client";
 import TodoContainer from "./funcitonBased/components/TodoContainer"
+import { BrowserRouter as Router } from "react-router-dom"
 
 //stylesheet
 import "./funcitonBased/App.css"
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
+  <React.StrictMode>
+    <Router>
       <TodoContainer />
-    </React.StrictMode>
+    </Router>
+  </React.StrictMode>,
   )
