08ad37b3ec11d91468eeac2fb54ac8a85fbee831
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-11-09 08:54:32 -0500

    Add componentDidMount with the local Storage

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -55,6 +55,15 @@ class TodoContainer extends React.Component {
           const temp = JSON.stringify(this.state.todos)
           localStorage.setItem("todos", temp)
         }
+      };
+      componentDidMount() {
+        const temp = localStorage.getItem("todos")
+        const loadedTodos = JSON.parse(temp)
+        if (loadedTodos) {
+          this.setState({
+            todos: loadedTodos
+          })
+        }
       }
 
       render() {
