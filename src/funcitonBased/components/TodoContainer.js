b7975f0684a91fa7c50abedb6ef79ca3cd878225
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-11-09 14:16:25 -0500

    Add useEffect to detect setTodos and todos changes

--- src/funcitonBased/components/TodoContainer.js
+++ src/funcitonBased/components/TodoContainer.js
@@ -1,89 +1,86 @@
-import React from "react"
-import TodosList from "./TodosList";
-import Header from "./Header";
-import InputTodo from "./InputTodo";
-import { v4 as uuidv4 } from "uuid";
-class TodoContainer extends React.Component {
-    state = {
-        todos: [],
-      };
-       
-       handleChange = id => {
-        this.setState(prevState => ({
-            todos: prevState.todos.map(todo => {
-              if (todo.id === id) {
-                return {
-                  ...todo,
-                  completed: !todo.completed,
-                }
-              }
-              return todo
-            }),
-          }))
-      };
-      delTodo = id => {
-        this.setState({
-            todos: [
-              ...this.state.todos.filter(todo => {
-                return todo.id !== id;
-              })
-            ]
-          });
-      };
-      addTodoItem = title => {
-        const newTodo = {
-            id: uuidv4(),
-            title: title,
-            completed: false
-          };
-          this.setState({
-            todos: [...this.state.todos, newTodo]
-          });
-      };
-      setUpdate = (updatedTitle, id) => {
-        this.setState({
-          todos: this.state.todos.map(todo => {
-            if (todo.id === id) {
-              todo.title = updatedTitle
-            }
-            return todo
-          }),
-        })
-      };
-      componentDidUpdate(prevProps, prevState) {
-        if(prevState.todos !== this.state.todos) {
-          const temp = JSON.stringify(this.state.todos)
-          localStorage.setItem("todos", temp)
-        }
-      };
-      componentDidMount() {
-        const temp = localStorage.getItem("todos")
-        const loadedTodos = JSON.parse(temp)
-        if (loadedTodos) {
-          this.setState({
-            todos: loadedTodos
-          })
-        }
-      };
-      componentWillUnmount() {
-        console.log("Cleaning up...")
-      }
+import React, { useState, useEffect } from "react"
+import Header from "./Header"
+import InputTodo from "./InputTodo"
+import TodosList from "./TodosList"
+import { v4 as uuidv4 } from "uuid"
 
-      render() {
-        return (
-          <div className="container">
-            <div className="inner">
-              <Header />
-              <InputTodo addTodoProps={this.addTodoItem} />
-              <TodosList
-                todos={this.state.todos}
-                handleChangeProps={this.handleChange}
-                deleteTodoProps={this.delTodo}
-                setUpdate={this.setUpdate}
-              />
-            </div>
-          </div>
-        );
-      }
+const TodoContainer = () => {
+  const [todos, setTodos] = useState([])
+
+  const handleChange = id => {
+    setTodos(prevState =>
+      prevState.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        }
+        return todo
+      })
+    )
+  }
+
+  const delTodo = id => {
+    setTodos([
+      ...todos.filter(todo => {
+        return todo.id !== id
+      }),
+    ])
+  }
+
+  const addTodoItem = title => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false,
+    }
+    setTodos([...todos, newTodo])
+  }
+
+  const setUpdate = (updatedTitle, id) => {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.title = updatedTitle
+        }
+        return todo
+      })
+    )
+  }
+
+  useEffect(() => {
+    console.log("test run")
+  
+    // getting stored items
+    const temp = localStorage.getItem("todos")
+    const loadedTodos = JSON.parse(temp)
+  
+    if (loadedTodos) {
+      setTodos(loadedTodos)
+    }
+  }, [])
+
+  useEffect(() => {
+    // storing todos items
+    const temp = JSON.stringify(todos)
+    localStorage.setItem("todos", temp)
+  }, [todos])
+  
+  return (
+    <div className="container">
+      <div className="inner">
+        <Header />
+        <InputTodo addTodoProps={addTodoItem} />
+        <TodosList
+          todos={todos}
+          handleChangeProps={handleChange}
+          deleteTodoProps={delTodo}
+          setUpdate={setUpdate}
+        />
+      </div>
+    </div>
+  )
 }
+
 export default TodoContainer
